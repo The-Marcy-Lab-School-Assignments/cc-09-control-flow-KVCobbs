876eaa566afc2def47f9b8482a3c21a5bfaef512
KVCobbs <kenyacobbs9001@gmail.com>
2021-10-27 14:35:24 +0000

    I got stuck on the first one because of else and elde if

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(int) {
+    if (int >= 5) {
+        console.log(true);
+     }else if(int <= 10) {
+        console.log(true);
+    } else{
+    console.log(false);
+  }
 }
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(int){
+  if (int >= 10) {
+      console.log(true);
+  } else if (int <= 30) {
+      console.log(true);
+  } else {
+     console.log(false);
 }
 
 //3
 function betweenThirtyAndFifty(){
-  
+    if (int >= 30) {
+        console.log(true);
+    } else if (int <= 50) {
+        console.log(true)
+    } else {
+  console.log(false)
 }
 
 
 //4
 function betweenThirtyFiftySeventy(){
-  
+  if (int >= 0 && 20 || int >= 50 && 70) {
+      console.log(true)
+  } else {
+      console.log(false)
+  }
 }
 
 //5
 function betweenTwentyFiftySixty(){
-  
+  if (int >= 0 && 20 || int >= 50 && 60)
 }
 
 //6
 function betweenTenTwentyThirty(){
   
 }
+
+betweenFiveAndTen(6)
